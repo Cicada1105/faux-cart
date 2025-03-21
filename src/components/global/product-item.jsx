2620c679e345d21515d2846a13217ec9d500fc10
Joshua Colvin <joshicolvin@gmail.com>
2025-03-21 00:04:38 -0400

    Product Item "Add to Cart" buttons are full width on mobile devices.
    Product Item cards now fill out the max container height, making the button, star rating and description move evenly to the bottom.

--- src/components/global/product-item.jsx
+++ src/components/global/product-item.jsx
@@ -3,13 +3,13 @@ import Rating from '@components/rating';
 function ProductItem({ title, price, discountPercentage = null, description, rating, reviews, thumbnail }) {
   let [ priceDollors, priceDecimal ] = price.toString().split('.');
   return (
-    <li className='rounded-xl text-left'>
+    <li className='rounded-xl text-left flex flex-col'>
       <div className='bg-[#f5f6f6] rounded-xl relative aspect-square'>
         <img className='hover:scale-100 hover:cursor-pointer scale-90 rounded-xl transition object-cover object-center w-full' src={ thumbnail } />
         <i className="hover:bg-[pink] hover:cursor-pointer bg-white p-2 transition rounded-full absolute right-4 top-4 fa-regular fa-heart"></i>
       </div>
-      <div className='mt-2 flex flex-col gap-y-2 items-start'>
-        <hgroup className='flex justify-between w-full font-bold'>
+      <div className='mt-2 flex-1 flex flex-col gap-y-2 items-start'>
+        <hgroup className='flex-1 flex justify-between w-full font-bold'>
           <h3>{ title }</h3>
           <p>
             <sup>$</sup>{ priceDollors }<sup>.{ priceDecimal }</sup>
@@ -24,7 +24,7 @@ function ProductItem({ title, price, discountPercentage = null, description, rat
           <Rating rating={rating} />
           <p className='text-sm'>({ reviews.length })</p>
         </div>
-        <button className='btn-outline-dark'>Add to Cart</button>
+        <button className='btn-outline-dark max-sm:w-full'>Add to Cart</button>
       </div>
     </li>
   );
