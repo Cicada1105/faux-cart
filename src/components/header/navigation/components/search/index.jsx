8c08ecf97067c1bbaff86f5068613d9815a415d6
Joshua Colvin <joshicolvin@gmail.com>
2025-03-22 18:56:22 -0400

    Added navigation responsiveness. Main navigation becomes a drop down on smaller screens, toggled by clicking hamburger icon.

--- src/components/header/navigation/components/search/index.jsx
+++ src/components/header/navigation/components/search/index.jsx
@@ -1,7 +1,7 @@
 
 function Search() {
   return (
-    <search className='relative flex-grow'>
+    <search className='relative hidden lg:block flex-grow'>
       <form action='/search'>
         <label htmlFor='search-product-field' className='sr-only'>Search Products</label>
         <input id='search-product-field' className='w-full px-4 py-2 outline-none border-green focus:border-blue border-2 rounded-full' type='search' name='q' placeholder='Search Product' />
--- src/components/header/navigation/index.jsx
+++ src/components/header/navigation/index.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import DarkLogo from '/dark-logo.svg';
 
 import Categories from './components/categories';
@@ -5,6 +7,8 @@ import Links from './components/links';
 import Search from './components/search';
 
 function Navigation() {
+  const [open, setOpen] = useState(false);
+
   const LINKS = [
     {
       path: './',
@@ -29,15 +33,27 @@ function Navigation() {
       text: 'Cart'
     }
   ]
+
   return (
-    <div className='flex gap-x-4 items-center shadow-md px-8 py-6 font-bold text-green'>
+    <div className='relative flex gap-x-4 justify-between items-center shadow-md px-8 py-6 font-bold text-green'>
       <img src={ DarkLogo } alt='Shopcart Logo' className='w-32'/>
-      <Categories />
-      <nav>
-        <Links links={ LINKS } />
-      </nav>
+      <div className={( open ? 'hidden' : 'flex' ).concat(' max-lg:flex-col max-lg:items-center max-lg:gap-y-4 max-lg:absolute max-lg:p-5 max-lg:mt-2 lg:flex top-full left-0 rounded-xl w-full bg-white')}>
+        <Categories />
+        <nav className='flex-grow'>
+          <ul className='flex max-lg:flex-col justify-around gap-y-4 gap-x-2'>
+            {
+              LINKS.map((link, i) => 
+                <li key={ i }>
+                  <a href={ link['path'] }>{ link['text'] }</a>
+                </li>
+              )
+            }
+          </ul>
+        </nav>
+      </div>
       <Search />
       <Links links={ ACCOUNT_LINKS } />
+      <i onClick={() => setOpen(!open)} className="lg:hidden fa-solid fa-bars"></i>
     </div>
   )
 }
